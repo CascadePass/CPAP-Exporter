dbe1504afc7851115382e070f3926d73e52c7db0
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-03-15 23:39:11 -0700

    Add tests
    For SignalInfo class.

--- CPAP-Exporter.Core/Aggregates/StreamingAggregate.cs
+++ CPAP-Exporter.Core/Aggregates/StreamingAggregate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CascadePass.CPAPExporter.Core.Aggregates
+﻿namespace CascadePass.CPAPExporter.Core.Aggregates
 {
     public abstract class StreamingAggregate<T>
     {
