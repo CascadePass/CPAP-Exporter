b560e6385b67e7efc10bb4fec3eefa5565cb3f25
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-03-20 21:02:15 -0700

    Add test
    More to come

---
+++ CPAP-Exporter.Tests/OpenFilesViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿namespace CascadePass.CPAPExporter.UI.Tests
+{
+    [TestClass]
+    public class OpenFilesViewModelTests
+    {
+        [TestMethod]
+        public void CanImportFrom_WithNonExistentFolder_ShouldReturnFalse()
+        {
+            var folderPath = @"C:\NonExistentFolder";
+            var viewModel = new OpenFilesViewModel();
+
+            Assert.IsFalse(viewModel.CanImportFrom(folderPath));
+        }
+
+        //TODO: Add real machine data
+    }
+}
